6f1811399b2dbf8f039e0d09d08d776e9e8800c6
Adam Reid <areid_van@hotmail.com>
2018-11-14 08:27:52 +0000

    Added height sensor
    Can query desk height from ultrasonic sensor.
    
    Can set height using height sensor, based on an estimated raise/lower
    speed. Speed would have to be calibrated for desk for accurate heights

--- standroid.py
+++ standroid.py
@@ -29,6 +29,64 @@ def moveDown(t):
     motionLock.release()
     
 
+GPIO_TRIGGER = 16
+GPIO_ECHO = 18
+
+def readDistance():
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+    BeginTime = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+        if StartTime - BeginTime > 0.1: raise IOError
+
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+        if StopTime - BeginTime > 0.1: raise IOError
+
+    TimeElapsed = StopTime - StartTime
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
+
+def distance():
+    count = 0
+    d = 0
+    for i in range(4):
+        try:
+            d += readDistance()
+            count += 1
+        except IOError:
+            pass
+
+    if count < 2: raise IOError("Device error")
+    return d/count
+
+@route('/height')
+def height():
+    return '%f' % distance()
+
+@route('/height/<h>')
+def setheight(h):
+    try:
+        h = float(h)
+    except ValueError:
+        abort(400)
+
+    d = distance()
+
+    if h > d:
+        t = (h - d)/8
+        return "moving up %f" % t
+    else:
+        t = (d - h)/8
+        return "moving down %f" % t
+
 @route('/up/<t>')
 def up(t):
     try:
@@ -63,6 +121,10 @@ def send_static(filename):
 
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
 global motionLock
 motionLock = threading.Lock();
 run(host='0.0.0.0', port=80, debug=True)
